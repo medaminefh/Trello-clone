d5e417d61ab6a2b02a93593d03504f1e39e02128
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-11-17 17:24:17 +0100

    user can change the title of the column

--- components/ModalColumnTitle.vue
+++ components/ModalColumnTitle.vue
@@ -1,49 +1,41 @@
 <script setup lang="ts">
-import type { Task } from "~/types";
+import type { Column } from "~/types";
 
-const props = defineProps<{ task: Task | null; isOpen: Boolean }>();
+const props = defineProps<{ column: Column; isOpen: Boolean }>();
 const emits = defineEmits<{
 	(e: "handelCloseModel"): void;
-	(e: "handelUpdateTask", task: Task): void;
-	(e: "handelDeleteTask"): void;
+	(e: "handelUpdateTitle", title: string): void;
+	(e: "handelDeleteColumn"): void;
 }>();
 
-const newTitle = ref();
-
-const handelUpdate = () => {
-	const newTask = props.task;
-	newTask!.title = newTitle.value;
-	emits("handelUpdateTask", newTask as Task);
+const handelDelete = () => {
+	emits("handelDeleteColumn");
 	emits("handelCloseModel");
 };
 
-const handelDelete = () => {
-	if (confirm(`❗Are you sure you want to delete this task❗`)) {
-		emits("handelDeleteTask");
-		emits("handelCloseModel");
-	}
+const handelUpdate = () => {
+	emits("handelUpdateTitle", newTitle.value);
+	emits("handelCloseModel");
 };
 
+const newTitle = ref("");
+
 watch(
-	() => props.task,
+	() => props.column,
 	(newValue) => {
 		newTitle.value = newValue?.title;
 	}
 );
 </script>
-
 <template>
 	<div
-		v-if="isOpen && task"
+		v-if="isOpen"
 		@click="emits('handelCloseModel')"
 		class="absolute z-10 bg-gray-300 bg-opacity-50 top-0 left-0 w-screen h-screen overflow-hidden flex justify-center items-center"
 	>
-		<div
-			@click.stop
-			class="relative rounded-xl w-[700px] h-[500px] bg-white overflow-y-auto flex flex-col justify-between"
-		>
+		<div @click.stop class="relative bg-white rounded-xl overflow-hidden">
 			<header class="flex justify-between bg-teal-700 p-4">
-				<h3 class="text-xl text-white">{{ task.id.substring(8) }}</h3>
+				<h3 class="text-xl text-white">{{ column.id.substring(8) }}</h3>
 				<button
 					@click="emits('handelCloseModel')"
 					class="text-2xl text-white font-bold"
@@ -52,16 +44,12 @@ watch(
 				</button>
 			</header>
 			<div class="p-10">
-				<div class="flex flex-col gap-4 mt-4">
-					<label>Title</label>
-					<textarea
-						rows="5"
-						v-model="newTitle"
-						class="outline-none border border-black px-4 py-2 w-full"
-					/>
-				</div>
+				<input
+					v-model="newTitle"
+					class="outline-none border border-black px-4 py-2 w-full rounded"
+				/>
 			</div>
-			<footer class="flex justify-center mt-4 gap-10 p-10">
+			<footer class="flex justify-center gap-10 p-4">
 				<button @click="emits('handelCloseModel')" class="text-gray-500">
 					Cancel
 				</button>
--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -73,32 +73,31 @@ const columns = ref<Column[]>([
 
 const alt = useKeyModifier("Alt");
 const selectedTask = ref<Task | null>(null);
-const selectedColumnId = ref<string | null>(null);
-const openModel = ref(false);
-const mouseStatus = ref({
-	start: 0,
-	isMouving: false,
-});
+const selectedColumn = ref<Column | null>(null);
+const openTaskModal = ref(false);
+const openColumnModal = ref(false);
 
-const handelOpenModel = (columnId: string, task: Task) => {
-	selectedColumnId.value = columnId;
+const handelOpenModel = (column: Column, task: Task) => {
+	selectedColumn.value = column;
 	selectedTask.value = task;
-	openModel.value = true;
+	openTaskModal.value = true;
 };
 
 const handelCloseModel = () => {
 	selectedTask.value = null;
-	openModel.value = false;
+	openTaskModal.value = false;
 };
 
 const handelUpdateTask = (task: Task) => {
-	const column = columns.value.find((col) => col.id == selectedColumnId.value)!;
+	const column = columns.value.find(
+		(col) => col.id == selectedColumn.value?.id
+	)!;
 	const tasks = column?.tasks;
 	const updatedTasks = tasks?.map((oldTask) =>
 		oldTask.id == selectedTask.value?.id ? task : oldTask
 	);
 
-	if (selectedColumnId.value) {
+	if (selectedColumn.value?.id) {
 		column.tasks = updatedTasks as Task[];
 	}
 };
@@ -123,19 +122,55 @@ const handelCreateTask = (columnId: string) => {
 };
 
 const handelDeleteTask = () => {
-	const column = columns.value.find((col) => col.id == selectedColumnId.value)!;
+	const column = columns.value.find(
+		(col) => col.id == selectedColumn.value?.id
+	)!;
 	const tasks = column.tasks;
 	const updatedTasks = tasks.filter(
 		(task) => task.id !== selectedTask.value?.id
 	);
 	column.tasks = updatedTasks;
 };
+
+const openChangeTitleModal = (column: Column) => {
+	openColumnModal.value = true;
+	selectedColumn.value = column;
+};
+
+const handelChangeColumnTitle = (title: string) => {
+	const newCols = columns.value.map((col) =>
+		col.id == selectedColumn.value?.id ? { ...col, title } : col
+	);
+	columns.value = newCols;
+};
+
+const handelDeleteColumn = () => {
+	if (confirm("Are you sure you want to delete this column!")) {
+		const newCols = columns.value.filter(
+			(col) => col.id !== selectedColumn.value?.id
+		);
+		columns.value = newCols;
+		selectedColumn.value = null;
+	}
+};
+
+const handelCloseColumnModal = () => {
+	selectedColumn.value = null;
+	openColumnModal.value = false;
+};
 </script>
 
 <template>
+	<ModalColumnTitle
+		:isOpen="openColumnModal"
+		:column="selectedColumn"
+		@handelCloseModel="handelCloseColumnModal"
+		@handelUpdateTitle="handelChangeColumnTitle"
+		@handelDeleteColumn="handelDeleteColumn"
+	/>
 	<ModalTask
 		:task="selectedTask"
-		:isOpen="openModel"
+		:isOpen="openTaskModal"
 		@handelCloseModel="handelCloseModel"
 		@handelUpdateTask="handelUpdateTask"
 		@handelDeleteTask="handelDeleteTask"
@@ -154,7 +189,9 @@ const handelDeleteTask = () => {
 				<div class="bg-gray-200 p-5 rounded min-w-[250px]">
 					<header class="font-bold mb-4">
 						<DragHandelIcon />
-						{{ column.title }}
+						<span @click="openChangeTitleModal(column)" class="cursor-pointer">
+							{{ column.title }}
+						</span>
 					</header>
 
 					<draggable
@@ -169,7 +206,7 @@ const handelDeleteTask = () => {
 							<div class="cursor-pointer">
 								<TrelloTask
 									:task="task"
-									@click="handelOpenModel(column.id, task)"
+									@click="handelOpenModel(column, task)"
 								/>
 							</div> </template
 					></draggable>
