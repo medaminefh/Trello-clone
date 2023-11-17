9984adbca3717d6bf80597bf83a672746283219c
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-11-17 14:50:33 +0100

    Add free palestine banner

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
+	<Header />
 	<div class="p-10 overflow-auto h-[100vh] bg-teal-400">
 		<h1 class="text-4xl text-white mb-10 font-bold text-center">
 			Trello Clone
---
+++ components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+	<p class="text-center text-white font-bold text-xl bg-teal-900 py-3">
+		🇵🇸 Free Palestine 🇵🇸
+	</p>
+</template>
